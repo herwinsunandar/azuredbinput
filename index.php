8e916eca54ef1b5dad802f4b39ce90bea9a0192c
herwinsunandar <herwin.sunandar@gmail.com>
2019-06-23 21:50:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,7 @@
  	h1 { font-size: 2em; }
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+ 
  </style>
   <style>
 #customers {
